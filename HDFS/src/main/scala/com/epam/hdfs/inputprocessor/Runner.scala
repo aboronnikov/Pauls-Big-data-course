75057b53c5d9e2bf761ecfaf08e163fd35a5a1f5
Pavel_Orekhov <Pavel_Orekhov@epam.com>
2018-11-16 16:42:07 +0300

    Extracted cmd line logic into a separate utility file.

--- HDFS/src/main/scala/com/epam/hdfs/inputprocessor/Runner.scala
+++ HDFS/src/main/scala/com/epam/hdfs/inputprocessor/Runner.scala
@@ -20,19 +20,6 @@ object Runner {
   private val Log = Logger.getLogger(Runner.getClass)
 
   /**
-   * Checks if there are enough arguments to run this program.
-   *
-   * @param cmdLine Cmd args.
-   * @return Boolean telling whether or not there are enough arguments to run this program.
-   */
-  private def areThereEnoughArguments(cmdLine: CommandLine): Boolean = {
-    cmdLine.hasOption(ArgConstants.SchemaPathArg) &&
-    cmdLine.hasOption(ArgConstants.NewFilePathArg) &&
-    cmdLine.hasOption(ArgConstants.CsvSeparatorArg) &&
-    cmdLine.hasOption(ArgConstants.CsvPathArg)
-  }
-
-  /**
    * The main function of this utility that converts csv to parquet format.
    */
   private def convertCsvToParquet(csvFilePath: String, csvSeparator: String, newFilePath: String, schemaFilePath: String): Unit = {
@@ -52,43 +39,24 @@ object Runner {
    * If they asked for help, then prints the tool's manual.
    * If they didn't provide good arguments, then exits the program and tells about the bad arguments.
    *
-   * @param cmdLine Cmd args.
-   * @param options Possible cmd options.
-   */
-  private def takeActionAccordingToUserInput(cmdLine: CommandLine, options: Options): Unit = {
-    if (cmdLine.hasOption(ArgConstants.HelpArg)) {
-      val formatter = new HelpFormatter
-      formatter.printHelp("CsvToParquetConverter", options)
-    } else if (areThereEnoughArguments(cmdLine)) {
-      val csvPath = cmdLine.getOptionValue(ArgConstants.CsvPathArg)
-      val csvSeparator = cmdLine.getOptionValue(ArgConstants.CsvSeparatorArg)
-      val newFilePath = cmdLine.getOptionValue(ArgConstants.NewFilePathArg)
-      val schemaPath = cmdLine.getOptionValue(ArgConstants.SchemaPathArg)
-      convertCsvToParquet(csvPath, csvSeparator, newFilePath, schemaPath)
-      Log.info(FeedbackMessageConstants.Success)
-    } else {
-      Log.info(FeedbackMessageConstants.BadArgsProblem)
-    }
-  }
-
-  /**
-   * The entry point of the program.
-   *
-   * @param args command line args.
+   * @param args user input
    */
   def main(args: Array[String]): Unit = {
     try {
-      val options = new Options
-      options
-        .addOption(ArgConstants.SchemaPathArg, true, "Specifies the path to the schema file. Required.")
-        .addOption(ArgConstants.CsvPathArg, true, "Specified the path to the csv file. Required.")
-        .addOption(ArgConstants.CsvSeparatorArg, true, "Specifies the separator used in the csv file. Required.")
-        .addOption(ArgConstants.NewFilePathArg, true, "Specifies the path to the new parquet file. Required.")
-        .addOption(ArgConstants.HelpArg, false, "Argument to call when you need help for this tool.")
-
-      val cmdParser = new DefaultParser
-      val cmdLine = cmdParser.parse(options, args)
-      takeActionAccordingToUserInput(cmdLine, options)
+      val cmdLine = CmdUtils.parseArgs(args)
+      if (cmdLine.hasOption(ArgConstants.HelpArg)) {
+        val formatter = new HelpFormatter
+        formatter.printHelp("CsvToParquetConverter", CmdUtils.options)
+      } else if (CmdUtils.areThereEnoughArguments(cmdLine)) {
+        val csvPath = cmdLine.getOptionValue(ArgConstants.CsvPathArg)
+        val csvSeparator = cmdLine.getOptionValue(ArgConstants.CsvSeparatorArg)
+        val newFilePath = cmdLine.getOptionValue(ArgConstants.NewFilePathArg)
+        val schemaPath = cmdLine.getOptionValue(ArgConstants.SchemaPathArg)
+        convertCsvToParquet(csvPath, csvSeparator, newFilePath, schemaPath)
+        Log.info(FeedbackMessageConstants.Success)
+      } else {
+        Log.info(FeedbackMessageConstants.BadArgsProblem)
+      }
     } catch {
       case e: NumberFormatException => Log.info(FeedbackMessageConstants.BadNumberFormat, e)
       case e: ParseException        => Log.info(FeedbackMessageConstants.BadArgsProblem, e)
