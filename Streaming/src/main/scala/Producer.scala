f7084f53996cf3acc7e4b44ec4cd6a4cc000511b
Pavel_Orekhov <Pavel_Orekhov@epam.com>
2018-11-19 19:26:04 +0300

    Rewrote producer to have multiple threads.

--- Streaming/src/main/scala/Producer.scala
+++ Streaming/src/main/scala/Producer.scala
@@ -1,4 +1,6 @@
+import java.io.{BufferedReader, FileInputStream, InputStreamReader}
 import java.util.Properties
+import java.util.concurrent.{CompletableFuture, Executors}
 
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig, ProducerRecord}
 import org.apache.log4j.Logger
@@ -9,39 +11,46 @@ object Producer {
   //private val Log = Logger.getLogger(MessageCallback.getClass)
 
   def main(args: Array[String]): Unit = {
-    val url = args(2)
-    val props = new Properties()
-    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, url)
-    props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
-    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
-    props.put(ProducerConfig.PARTITIONER_CLASS_CONFIG, "IdPartitioner")
-    ProducerConfig.
-    var kafkaProducer: KafkaProducer[String, String] = null
-    println("created config")
-    try {
-      kafkaProducer = new KafkaProducer[String, String](props)
-      println("created kafka producer")
-      //Log.info("created kafka producer")
-      val fileName = args(0)
-      val topic = args(1)
-      val fileSource = Source.fromFile(fileName)
-        .getLines()
-        .drop(1)
-        .foreach(
-          line => {
-           // Log.info("LINE")
-            println("LINE: " + line)
-            val key = line.substring(0, line.indexOf(','))
-            val record = new ProducerRecord[String, String](topic, "123", line)
-            kafkaProducer.send(record, MessageCallback)
+    if (args.length == 4) {
+      val url = args(1)
+      val props = new Properties()
+      props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, url)
+      props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
+      props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
+      props.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, "600000")
+      props.put(ProducerConfig.BATCH_SIZE_CONFIG,"100000")
+      props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, "66554432")
+
+      val kafkaProducer = new KafkaProducer[String, String](props)
+      val nThreads = Integer.parseInt(args(2))
+      val topic = args(0)
+      val futures = new Array[CompletableFuture[Unit]](nThreads)
+      val executor = Executors.newFixedThreadPool(nThreads)
+      val bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(args(3))))
+      for (i <- 0 until nThreads) {
+        futures(i) = CompletableFuture.supplyAsync(() => {
+          var line: String = null
+          bufferedReader.synchronized {
+            line = bufferedReader.readLine()
           }
-        )
-    } catch {
-      case e: Exception => //Log.error("THE ERROR:" + e.getMessage)
-    } finally {
-      if (kafkaProducer != null) {
-        kafkaProducer.close()
+          while (line != null) {
+            val key = line.substring(0, line.indexOf(","))
+            val record = new ProducerRecord[String, String](topic, key, "Thread num: " + i + " ==> " + line)
+            kafkaProducer.send(record, MessageCallback)
+            bufferedReader.synchronized {
+              line = bufferedReader.readLine()
+            }
+          }
+
+        }, executor)
+        futures(i).exceptionally(e => {
+          println(e.getMessage)
+        })
       }
+      CompletableFuture.allOf(futures: _*)
+      //kafkaProducer.close()
+    } else {
+      println("First arg is url, second arg is nThreads, third arg is topic")
     }
   }
 }
